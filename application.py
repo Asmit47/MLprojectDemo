3f4fea23090a0fe4e272c5c7cc5408459f685d00
Asmit Kaushal <asmitkaushal47@gmail.com>
2026-03-28 02:51:59 +0530

    debug + modified dependencies

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from flask import Flask, request, render_template
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
-app = application
+application = app
 
 
 @app.route("/")
